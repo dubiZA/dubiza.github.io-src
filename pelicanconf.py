42c66410c65035699f1c754b164ea5ef79d9d380
dubiZA <darius@dubiza.dev>
2021-03-22 09:14:01 -0600

    Add CNAME file

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,6 +19,9 @@ DEFAULT_METADATA = {
 
 FILENAME_METADATA = '(?P<title>.*)'
 
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -28,13 +31,11 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/dariushall/'),
+          ('GitHub', 'https://github.com/dubiZA'),)
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
