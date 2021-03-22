eeea5a45d6425154bca5e8d49eb6bcb043940b3c
dubiZA <darius@dubiza.dev>
2021-03-22 09:04:08 -0600

    Add about page

---
+++ content/pages/about.md
@@ -0,0 +1,5 @@
+Status: published
+
+Hi, I'm Darius. This is my space on the bit world wide web. I hope you enjoy it. By workday, I'm a SecOps Team Lead/Cloud Security Engineer. The rest of the time, I'm a husband, father, and tech enthusiast.
+
+The content on this site is where I chronicle my learning and share any projects I have been working on in the world of AWS, Python Cybersecurity, and anything else I dabble in that should probably be documented.
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Darius'
+SITENAME = 'dariushall.com'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Denver'
+
+# Defaults
+DEFAULT_LANG = 'en'
+DEFAULT_DATE = 'fs'
+DEFAULT_PAGINATION = False
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
+FILENAME_METADATA = '(?P<title>.*)'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
