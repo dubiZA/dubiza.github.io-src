3361c46068d882dd514ec0bffd9ed0eabbc9b419
dubiZA <darius@dubiza.dev>
2021-03-25 06:46:44 -0600

    Update Flex theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,59 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from datetime import datetime
 
 AUTHOR = 'Darius'
+SITEURL = 'http://localhost:8000'
 SITENAME = 'Darius Hall'
-SITEURL = 'http://localhost:8000/'
-
-PATH = 'content'
-
-TIMEZONE = 'America/Denver'
-
-# Defaults
-DEFAULT_LANG = 'en'
-DEFAULT_DATE = 'fs'
-DEFAULT_PAGINATION = False
-DEFAULT_METADATA = {
-    'status': 'draft',
-}
-
-FILENAME_METADATA = '(?P<title>.*)'
-
-STATIC_PATHS = ['images/', 'extra/robots.txt', 'extra/favicon.ico', 'extra/CNAME']
-EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-    'extra/CNAME': {'path': 'CNAME'},
-}
-
-PLUGIN_PATHS = ['./pelican-plugins']
-PLUGINS = ['sitemap', 'feed_summary']
-
-# Theme config
-THEME = 'themes/Flex'
-SITELOGO = '/images/profile.jpg'
 SITETITLE = 'Darius Hall'
 SITESUBTITLE = 'My piece of the Internet'
+SITEDESCRIPTION = 'The epic chronicles of Darius\' forays in to tech, coding and, well, all the things'
+SITELOGO = SITEURL + '/images/profile.jpg'
+FAVICON = SITEURL + '/images/favicon.ico'
+BROWSER_COLOR = '#333'
+PYGMENTS_STYLE = 'monokai'
 
-SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.6,
-        'indexes': 0.6,
-        'pages': 0.5,
-    },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly',
-    }
+ROBOTS = 'index, follow'
+
+THEME = 'themes/Flex'
+PATH = 'content'
+OUTPUT_PATH = 'blog/'
+TIMEZONE = 'America/Denver'
+
+DISABLE_URL_HASH = True
+
+# PLUGIN_PATHS = ['./pelican-plugins']
+# PLUGINS = ['sitemap', 'feed_summary']
+
+DEFAULT_LANG = 'en'
+OG_LOCALE = 'en_US'
+LOCALE = 'en_US'
+
+DATE_FORMATS = {
+    'en': '%B %d, %Y',
 }
-
-MAIN_MENU = True
-MENUITEMS = (
-    ('Categories', '/categories'),
-    ('Tags', '/tags')
-)
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -62,13 +40,83 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
 
 # Social widget
-SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/dariushall/'),
-          ('GitHub', 'https://github.com/dubiZA'),)
+SOCIAL = (
+    ('linkedin', 'https://www.linkedin.com/in/dariushall/'),
+    ('github', 'https://github.com/dubiZA'),
+    ('rss', '/blog/feeds/all.atom.xml'),
+)
+
+MENUITEMS = (
+    ('Archives', '/archives'),
+    ('Categories', '/categories'),
+    ('Tags', '/tags')
+)
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa',
+    'icon': False,
+    'language': 'en_US',
+}
+
+COPYRIGHT_YEAR = datetime.now().strftime('%Y')
+DEFAULT_PAGINATION = False
+
+# DISQUS_SITENAME = ''
+# ADD_THIS_ID = ''
+
+STATIC_PATHS = ['images/', 'extra/robots.txt', 'extra/favicon.ico', 'extra/CNAME']
+
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/CNAME': {'path': 'CNAME'},
+}
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+USE_LESS = True
+
+CUSTOM_CSS = 'static/custom.css'
+
+# Other Defaults
+DEFAULT_DATE = 'fs'
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
+FILENAME_METADATA = '(?P<title>.*)'
+
+# SITEMAP = {
+#     'format': 'xml',
+#     'priorities': {
+#         'articles': 0.6,
+#         'indexes': 0.6,
+#         'pages': 0.5,
+#     },
+#     'changefreqs': {
+#         'articles': 'monthly',
+#         'indexes': 'daily',
+#         'pages': 'monthly',
+#     }
+# }
+
+USE_FOLDER_AS_CATEGORY = True
+
+# Blogroll
+LINKS = (
+    ('AWS', '/category/aws'),
+    ('Pelican', 'https://getpelican.com/'),
+    ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+)
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
